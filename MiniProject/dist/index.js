9c9feab93b0c895efae97ede701b4ed8c20a5411
XinyuP <xinyup@umich.edu>
2023-02-12 23:42:38 -0500

    DOM, lib compiler option, TypeScript Non-Null assertion operator(!), and (?) to check for null

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello !!!!!");
+// document; // TypeScript automatically knows about the document object and its type
+const btn = document.getElementById('btn');
+console.log(btn);
+// add ? here so if btn is null, we don't addEventListener
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => alert('CLICK!'));
+// or:
+// this is a typescript thing, js doesn't have this
+// non-null assertion operator  !
+const btnn = document.getElementById('btn');
+// ! tell typescript to stop worrying, this is guaranteed not to be null
+// a little bit risky though
+// we only use ! when we know for sure that a value will be found or that it won't be null
