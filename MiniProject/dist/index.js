bbcfd4efd7e44261b5cf95e400d180df4f2799fd
XinyuP <xinyup@umich.edu>
2023-02-13 00:17:17 -0500

    working with event, SubmitEvent, form

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -2,31 +2,44 @@
 // document; // TypeScript automatically knows about the document object and its type
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
-btn.addEventListener('click', () => {
-    // input.value
-    // before using (as HTMLInputElement) --> error: Property 'value' does not exist on type 'HTMLElement'
-    // (input as HTMLInputElement).value
-    alert(input.value);
-    input.value = '';
-    // or we can also do this:
-    input.value;
-});
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-// add ? here so if btn is null, we don't addEventListener
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => alert('CLICK!'));
-// or:
-// this is a typescript thing, js doesn't have this
-///////// non-null assertion operator  ! /////////
-const btnn = document.getElementById('btn');
-// ! tell typescript to stop worrying, this is guaranteed not to be null
-// a little bit risky though
-// we only use ! when we know for sure that a value will be found or that it won't be null
-///////// Type assertion /////////
-// Sometimes, we might have more specific info about a value's type, and we want to make sure
-// TypeScript knows it too --> we can assert a value's type by using the 'as' ketword
-// followed by the specific type we want to assert
-let mystery = 'Hello world!';
-// const len = mystery.length  //error: 'mystery' is of type 'unknown'
-const len = mystery.length;
+// const form = document.querySelector("#todoform")
+const form = document.querySelector('form');
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+};
+form.addEventListener("submit", handleSubmit);
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log("submit")
+// })
+// TypeScript can figure out the type of e by itself when we has an inline anonymous function 
+///////////////////////////////////////////////////////////////////
+// btn.addEventListener('click', () => {
+// 	// input.value
+// 	// before using (as HTMLInputElement) --> error: Property 'value' does not exist on type 'HTMLElement'
+// 	// (input as HTMLInputElement).value
+// 	alert(input.value);
+//     input.value = '';
+//     // or we can also do this:
+//     (<HTMLInputElement> input).value
+// });
+// //////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
+// // add ? here so if btn is null, we don't addEventListener
+// btn?.addEventListener('click', () => alert('CLICK!'));
+// // or:
+// // this is a typescript thing, js doesn't have this
+// ///////// non-null assertion operator  ! /////////
+// const btnn = document.getElementById('btn')!;
+// // ! tell typescript to stop worrying, this is guaranteed not to be null
+// // a little bit risky though
+// // we only use ! when we know for sure that a value will be found or that it won't be null
+// ///////// Type assertion /////////
+// // Sometimes, we might have more specific info about a value's type, and we want to make sure
+// // TypeScript knows it too --> we can assert a value's type by using the 'as' ketword
+// // followed by the specific type we want to assert
+// let mystery: unknown = 'Hello world!';
+// // const len = mystery.length  //error: 'mystery' is of type 'unknown'
+// const len = (mystery as string).length;
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -2,42 +2,60 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 
-btn.addEventListener('click', () => {
-	// input.value
-	// before using (as HTMLInputElement) --> error: Property 'value' does not exist on type 'HTMLElement'
+// const form = document.querySelector("#todoform")
+const form = document.querySelector('form')!;
 
-	// (input as HTMLInputElement).value
-	alert(input.value);
-    input.value = '';
+const handleSubmit = (e: SubmitEvent) => { //TypeScript does not know the type of e here
+    e.preventDefault();
+    console.log("submit")
+}
 
-    // or we can also do this:
-    (<HTMLInputElement> input).value
-});
+form.addEventListener("submit", handleSubmit)
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log("submit")
+// })
+// TypeScript can figure out the type of e by itself when we has an inline anonymous function 
 
 
 
 
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
 
-// add ? here so if btn is null, we don't addEventListener
-btn?.addEventListener('click', () => alert('CLICK!'));
+///////////////////////////////////////////////////////////////////
+// btn.addEventListener('click', () => {
+// 	// input.value
+// 	// before using (as HTMLInputElement) --> error: Property 'value' does not exist on type 'HTMLElement'
 
-// or:
-// this is a typescript thing, js doesn't have this
-///////// non-null assertion operator  ! /////////
-const btnn = document.getElementById('btn')!;
-// ! tell typescript to stop worrying, this is guaranteed not to be null
-// a little bit risky though
-// we only use ! when we know for sure that a value will be found or that it won't be null
+// 	// (input as HTMLInputElement).value
+// 	alert(input.value);
+//     input.value = '';
 
-///////// Type assertion /////////
-// Sometimes, we might have more specific info about a value's type, and we want to make sure
-// TypeScript knows it too --> we can assert a value's type by using the 'as' ketword
-// followed by the specific type we want to assert
+//     // or we can also do this:
+//     (<HTMLInputElement> input).value
+// });
 
-let mystery: unknown = 'Hello world!';
-// const len = mystery.length  //error: 'mystery' is of type 'unknown'
+// //////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-const len = (mystery as string).length;
+// // add ? here so if btn is null, we don't addEventListener
+// btn?.addEventListener('click', () => alert('CLICK!'));
+
+// // or:
+// // this is a typescript thing, js doesn't have this
+// ///////// non-null assertion operator  ! /////////
+// const btnn = document.getElementById('btn')!;
+// // ! tell typescript to stop worrying, this is guaranteed not to be null
+// // a little bit risky though
+// // we only use ! when we know for sure that a value will be found or that it won't be null
+
+// ///////// Type assertion /////////
+// // Sometimes, we might have more specific info about a value's type, and we want to make sure
+// // TypeScript knows it too --> we can assert a value's type by using the 'as' ketword
+// // followed by the specific type we want to assert
+
+// let mystery: unknown = 'Hello world!';
+// // const len = mystery.length  //error: 'mystery' is of type 'unknown'
+
+// const len = (mystery as string).length;
