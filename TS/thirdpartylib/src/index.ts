ca1ce9e4b2ffa4999f7fa6c187bb235adfe9f1a1
XinyuP <xinyup@umich.edu>
2023-02-13 15:29:54 -0500

    third party lib axios

---
+++ TS/thirdpartylib/src/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+axios
+	.get('https://jsonplaceholder.typicode.com/users/1')
+    .then((res) => {
+        
+        console.log('woo');
+        console.log(res.data);
+	})
+	.catch((e) => {
+		console.log('error', e);
+	});
