4ad91663afe9790bbd6959b1bc1bbd82bc512b54
XinyuP <xinyup@umich.edu>
2023-02-12 23:53:22 -0500

    type assertion

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -6,8 +6,15 @@ console.log(btn);
 btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => alert('CLICK!'));
 // or:
 // this is a typescript thing, js doesn't have this
-// non-null assertion operator  !
+///////// non-null assertion operator  ! /////////
 const btnn = document.getElementById('btn');
 // ! tell typescript to stop worrying, this is guaranteed not to be null
 // a little bit risky though
 // we only use ! when we know for sure that a value will be found or that it won't be null
+///////// Type assertion /////////
+// Sometimes, we might have more specific info about a value's type, and we want to make sure
+// TypeScript knows it too --> we can assert a value's type by using the 'as' ketword
+// followed by the specific type we want to assert
+let mystery = "Hello world!";
+// const len = mystery.length  //error: 'mystery' is of type 'unknown'
+const len = mystery.length;
