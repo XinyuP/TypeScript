9420c5c23abcad998228e743fd44590df635f33e
XinyuP <xinyup@umich.edu>
2023-02-12 21:43:56 -0500

    configuring: --watch -w watch mode auto complile, tsc-->compile all files in the folder

---
+++ TS/configuring/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const norma = {
+    breed: 'Silkie',
+    eggs: 4,
+    name: 'Norma',
+    age: 2,
+};
+// watch mode, auto compile
+// tsc -- watch
+// tsc -w ---> auto compile all the file in the folder
+// use only tsc will compile all the file in the folder 
