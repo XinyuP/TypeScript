ec4fd7df4021624715ae071d8de2e2500c6e717b
XinyuP <xinyup@umich.edu>
2023-02-13 04:54:36 -0500

    write first generic --> identity

--- TS/generics/dist/index.js
+++ TS/generics/dist/index.js
@@ -1,8 +1,45 @@
 "use strict";
+// -------------- built-in generic ----------------- //
 // const nums: number[] = [];
 const nums = [];
 const colors = [];
-const inputEl = document.querySelector("#username");
+const inputEl = document.querySelector('#username');
 console.dir(inputEl);
-inputEl.value = "Hacked!";
-const btn = document.querySelector(".btn");
+inputEl.value = 'Hacked!';
+const btn = document.querySelector('.btn');
+// ------------- create our own generic ----------- //
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+// wrong:
+// function identity(item: any): any {
+//     return item;
+// }
+// we want to return the type same as the type we pass to it
+// generic function:
+function identity(item) {
+    return item;
+}
+// we can call Type whatever we want 
+function identity2(item) {
+    return item;
+}
+function identity3(item) {
+    return item;
+}
+function identity4(item) {
+    return item;
+}
+// we established a relationship here --> whatever input type is, it will also be the return type
+// I can customize this identity function to work with different types 
+// It's a generic version that just says some type
+identity('blaire'); // automatically know it will return a
+identity(true);
+identity(7);
+identity({ name: 'oreo', breed: 'american short hair' });
--- TS/generics/src/index.ts
+++ TS/generics/src/index.ts
@@ -1,16 +1,58 @@
+// -------------- built-in generic ----------------- //
 // const nums: number[] = [];
 const nums: Array<number> = [];
 const colors: Array<string> = [];
 
-
-const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+const inputEl = document.querySelector<HTMLInputElement>('#username')!;
 console.dir(inputEl);
-inputEl.value = "Hacked!"
-const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+inputEl.value = 'Hacked!';
+const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+
+// ------------- create our own generic ----------- //
+function numberIdentity(item: number): number {
+	return item;
+}
+
+function stringIdentity(item: string): string {
+	return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+	return item;
+}
+
+// wrong:
+// function identity(item: any): any {
+//     return item;
+// }
+
+// we want to return the type same as the type we pass to it
+// generic function:
+function identity<Type>(item: Type): Type {
+	return item;
+}
+// we can call Type whatever we want 
+function identity2<A>(item: A): A {
+	return item;
+}
+function identity3<T>(item: T): T {
+	return item;
+}
+function identity4<generic>(item: generic): generic {
+	return item;
+}
+// we established a relationship here --> whatever input type is, it will also be the return type
+// I can customize this identity function to work with different types 
+// It's a generic version that just says some type
 
 
+identity<string>('blaire'); // automatically know it will return a
+identity<boolean>(true);
+identity<number>(7);
 
+interface Cat {
+	name: string;
+	breed: string;
+}
 
-
-
-
+identity<Cat>({ name: 'oreo', breed: 'american short hair' });
